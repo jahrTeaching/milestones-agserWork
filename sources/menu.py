68a2d1b0d77f069711bd49c2b2c89611bdf446a0
Andres <andres.gserrano@alumnos.upm.es>
2022-10-06 16:50:17 +0200

    feat: Upgrade main menu to new menu

--- sources/menu.py
+++ sources/menu.py
@@ -1,6 +1,10 @@
 # Menu selection of milestone
 
 
+import common
+
+from common.menu import Menu
+
 from milestone1 import menu as m1
 from milestone2 import menu as m2
 from milestone3 import menu as m3
@@ -11,38 +15,11 @@ from milestone6 import menu as m6
 
 
 if __name__ == "__main__":
-    # Build the list of milestones.
-    milestones = []
+    # Build the main menu
+    menu = Menu()
 
-    milestones.append(("Simple orbit methods",   m1.menu))
-    milestones.append(("Composed orbit methods", m2.menu))
-    milestones.append(("", m3.menu))
-    milestones.append(("", m4.menu))
-    milestones.append(("", m5.menu))
-    milestones.append(("", m6.menu))
+    menu.additem("milestone1", 1, "M1 - Simple orbit methods",      m1.menu)
+    menu.additem("milestone2", 2, "M2 - Composed orbit methods",    m2.menu)
+    menu.additem("milestone3", 3, "M3 - Richardson relative error", m3.menu)
 
-    # Build the input message.
-    message = "Select milestone:\n"
-
-    for i in range(0, len(milestones)):
-        message += f"  [{i+1}] Milestone {i+1}: {milestones[i][0]}\n"
-
-    message += "  [Q] Quit\n\n>> "
-    
-    while True:
-        string = input(message)
-
-        if string == "q" or string == "Q":
-            break
-
-        if not string.isnumeric():
-            print(f"Input not recognized: '{string}'")
-            continue
-
-        select = int(string)
-
-        if select < 1:
-            print("Minimum index is 1")
-            continue
-
-        milestones[select-1][1]()
+    menu.menu()
