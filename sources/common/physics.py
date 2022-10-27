8b3d98772ebfc5e072603e4d7c8504d24bbf483b
Andres <andres.gserrano@alumnos.upm.es>
2022-10-27 21:37:17 +0200

    feat: First correct NBody implementation

--- sources/common/physics.py
+++ sources/common/physics.py
@@ -3,6 +3,7 @@
 
 
 from ctypes import c_double
+from common.color import hue2rgb
 from common.config import Config
 from common.schemes import RungeKutta
 from functools import reduce
@@ -10,7 +11,7 @@ from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 from multiprocessing import Array, Pool, cpu_count as ncpus
 from multiprocessing.sharedctypes import Array
-from numpy import array, empty, frombuffer, size
+from numpy import array, empty, frombuffer, size, reshape
 from numpy.linalg import norm
 from numpy.typing import ArrayLike
 from random import random
@@ -41,31 +42,17 @@ def KeplerForce(U: ArrayLike, t: float):
 # N-Body problem solver.
 def NBody(cfg: Config):
     # Extract the base arguments.
-    num = cfg.getitem('num').value()
     steps = cfg.getitem('steps').value()
+    num = cfg.getitem('num').value()
     dt = cfg.getitem('dt').value()
 
-    # Calculate the number of processes needed.
-    nproc = int(num / ncpus()) + 1
-
-    print(f"Number of cores available: {ncpus()}")
-    print(f"Number of items per core: {nproc}")
-
-    # Create the shared array.
-    buffer = Array(c_double, 4 * num * steps)
-    npbuffer = frombuffer( buffer.get_obj() )
-    bodies = npbuffer.reshape((steps, num, 4))
-
-    # Populate the initial step.
+    # Create the array and populate the initial step.
+    bodies = empty((steps, num, 4))
     bodies[0] = array( [[random() for _ in range(4)] for _ in range(num)] )
 
-    # Calculate the N-Body problem - Not parallelized.
-    #NBodyThread(0, num, num, dt, bodies)
-
-    # Test N-Body with only two bodies.
-    bodies = array( [ [ [0.0 for _ in range(4)] for _ in range(2) ] for _ in range(20) ] )
-    bodies[0] = array( [[1.0, 0.0, 0.0, 0.0], [-1.0, 0.0, 0.0, 0.0]] )
-    NBodyThread(0, 2, 2, dt, bodies)
+    # Iterate over time.
+    for step in range(1, steps):
+        bodies[step] = RungeKutta(bodies[step-1], dt, 0.0, NBodyForce)
 
     # Get the X position.
     X = [ [body[0] for body in step] for step in bodies ]
@@ -74,20 +61,11 @@ def NBody(cfg: Config):
 
     # Transform the V array into colors.
 
-    # HUE to RGB transformation function.
-    r = lambda v: (5 + (v * 6)) % 6
-    g = lambda v: (3 + (v * 6)) % 6
-    b = lambda v: (1 + (v * 6)) % 6
-
-    c = lambda k: 1.0 - max( min( [k, 4-k, 1] ), 0 )
-
-    rgb = lambda v: (int(c(r(v))), int(c(g(v))), int(c(b(v))))
-
     # Maximum velocity value.
     Vmax = max( max(V[:]) )
 
     # Color map.
-    C = [ [rgb(Vel / Vmax) for Vel in step] for step in V ]
+    C = [ [hue2rgb(Vel / Vmax) for Vel in step] for step in V ]
 
     # Display animated problem.
     fig, ax = plt.subplots(figsize = (8, 8))
@@ -101,33 +79,35 @@ def NBody(cfg: Config):
     plt.show()
 
 
-def NBodyThread(s: int, e: int, num: int, dt: float, bodies):
-    # Get number of steps.
-    steps = len(bodies)
+# Calculates the derivatives in a step of the N-Body problem.
+def NBodyForce(U: ArrayLike, t: ArrayLike):
+    # Get the size of the physical world.
+    Nb = int( len(U)    )
+    Nc = int( len(U[0]) )
 
-    # Extract function.
-    mass = lambda U: (U[0], U[1])
+    # Get position and velocity pointers.
+    Us = reshape(U, (Nb, Nc))
+    r = reshape(Us[:, :2], (Nb, 2))
+    v = reshape(Us[:, 2:], (Nb, 2))
 
-    # Create a sum lambda.
-    sum = lambda x, y: (x[0] + y[0], x[1] + y[1])
+    # Get derivative pointers.
+    F = empty(size(U))
+    Fs = reshape(F, (Nb, Nc))
+    dr = reshape(Fs[:, :2], (Nb, 2))
+    dv = reshape(Fs[:, 2:], (Nb, 2))
 
-    # Create a fake U for modifications.
-    fake = array( [0.0 for _ in range(4)] )
+    # Calculate derivatives.
+    dr[:, :] = v[:, :]
 
-    for step in range(1, steps):
-        # Calculate the whole center of mass.
-        red = reduce( sum, map( mass, bodies[step-1] ) )
-        (cmx, cmy) = red
+    # Start iteration.
+    for i in range(Nb):
+        dv[:, :] = 0
 
-        for i in range(s, e):
-            # Load the new fake vector.
-            fake = bodies[step-1, i]
+        for j in range(Nb):
+            if i == j:
+                continue
+            
+            D = r[j, :] - r[i, :]
+            dv[:, :] = D / norm( D ** 3.0 )
 
-            # Calculate new center of mass.
-            mx, my = (cmx - fake[0]), (cmy - fake[1])
-
-            # Create the new fake vector.
-            #fake[0] = fake[0] - mx
-            #fake[1] = fake[1] - my
-
-            bodies[step,i,:] = RungeKutta( fake, dt, 0.0, KeplerForce )
+    return reshape(F, (Nb, Nc))
