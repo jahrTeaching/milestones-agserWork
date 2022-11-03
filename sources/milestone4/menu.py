a57b643f16fa63f61b3d483044829da32b789769
Andres <andres.gserrano@alumnos.upm.es>
2022-11-03 12:21:48 +0100

    feat: Complete milestone 4

--- sources/milestone4/menu.py
+++ sources/milestone4/menu.py
@@ -2,36 +2,51 @@
 
 
 
-def empty():
-    pass
+from common.config import Config
+from common.menu import Menu
+
+from common import schemes
+from common.config import Config
+from common.stability import RegionEstabilidad
+from matplotlib import pyplot as plt
+from numpy import array, linspace
+from typing import Callable
+
+
 
 def menu():
-    # Build the list of methods.
-    methods = [("Empty", empty)]
+    menu = Menu()
 
+    menu.additem("eu", 1, "Stability Region of Euler",          lambda : region(schemes.Euler)        )
+    menu.additem("ie", 2, "Stability Region of Inverse Euler",  lambda : region(schemes.EulerInverso) )
+    menu.additem("ie", 3, "Stability Region of Crank Nicolson", lambda : region(schemes.CrankNicolson))
+    menu.additem("ie", 4, "Stability Region of Runge-Kutta 4",  lambda : region(schemes.RungeKutta)   )
+    menu.additem("ie", 5, "Stability Region of Leap-Frog",      lambda : region(schemes.LeapFrog)     )
 
-    # Build the input message.
-    message = "Select orbit method:\n"
+    menu.menu()
 
-    for i in range(0, len(methods)):
-        message += f"  [{i+1}] {methods[i][0]}\n"
+def region(scheme: Callable):
+    # Default configuration.
+    N = 500
+    X = linspace(-6, 6, N)
+    Y = linspace(-6, 6, N)
 
-    message += "  [B] Back\n\n>> "
+    # Calculate the stability region.
+    region = RegionEstabilidad(X, Y, scheme)
 
-    while True:
-        s = input(message)
+    # Plot the stability region.
+    CSF = plt.contourf(X, Y, region, levels=[0,1], colors=['#E0E0E0'])
+    #CS = plt.contour(X, Y, region, levels = [0.25, 0.5, 0.75, 1, 1.25, 1.5])
+    CS = plt.contour(X, Y, region, levels = [0.1 * (i+1) for i in range(15)])
 
-        if s == "b" or s == "B":
-            return
+    plt.title(f'Region de Estabilidad Absoluta de {scheme.__name__}')
+    plt.xlabel('Re(|r|)')
+    plt.ylabel('Im(|r|)')
 
-        if not s.isnumeric():
-            print(f"Input not recognized: '{string}'")
-            continue
+    plt.xlim([-4, 4])
+    plt.ylim([-4, 4])
 
-        select = int(s)
+    plt.legend(loc = "lower left")
 
-        if select < 1:
-            print(f"Minimum index is 1")
-            continue
-
-        methods[select-1][1]()
+    plt.grid()
+    plt.show()
